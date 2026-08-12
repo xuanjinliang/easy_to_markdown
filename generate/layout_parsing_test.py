769155f2b18623e1cd085828b7889b4222725ee9
xuanjinliang <jinliang.xuan@mobvista.com>
2026-08-12 17:53:12 +0800

    add code

--- generate/layout_parsing_test.py
+++ generate/layout_parsing_test.py
@@ -66,8 +66,7 @@ class TestLayout(unittest.IsolatedAsyncioTestCase):
             Path(os.path.join(output_dir, "pdf_image")).glob("page_*.webp"),
             key=lambda p: int(re.search(r'\d+', p.stem).group()))
 
-        # webp_files = webp_files[:2]
-        webp_files = [webp_files[1]]
+        webp_files = webp_files[:2]
         image_list = []
         for i, item in enumerate(webp_files):
             image_list.append(
