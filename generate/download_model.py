65b08de6d61c518cd5327ce3bf497fea70a6b4cc
xuanjinliang <jinliang.xuan@mobvista.com>
2026-08-17 16:04:14 +0800

    add code

--- generate/download_model.py
+++ generate/download_model.py
@@ -1,14 +1,80 @@
+from transformers.processing_utils import transformers_module
+
 import pkg
 import os
-from huggingface_hub import hf_hub_download
+from pathlib import Path
+from huggingface_hub import snapshot_download
+import logging
+from logging import NullHandler
+
+logger = logging.getLogger(__name__)
+logger.addHandler(NullHandler())
+
 
 def download_model(repo_id: str, filename: str):
-    os.makedirs(pkg.ModelDir, exist_ok=True)
+    output_dir = Path(os.path.join(pkg.ModelDir, filename))
+    output_dir.parent.mkdir(parents=True, exist_ok=True)
 
-    model_path = hf_hub_download(
+    model_path = snapshot_download(
         repo_id=repo_id,
-        filename=filename,
-        cache_dir=os.path.join(pkg.ModelDir, "cache")
+        local_dir=os.path.join(pkg.ModelDir, filename),
     )
 
-    print(model_path)
+    logger.info(f"{repo_id} --> {str(model_path)}")
+
+
+def request_model_config() -> list[dict[str, str]]:
+    config = [
+        {
+            "repo_id": "PaddlePaddle/PP-DocLayoutV3",
+            "filename": "paddle/PP-DocLayoutV3",
+        },
+        {
+            "repo_id": "PaddlePaddle/PaddleOCR-VL-1.6",
+            "filename": "paddle/PP-DocLayoutV3",
+        },
+        {
+            "repo_id": "PaddlePaddle/PP-LCNet_x1_0_table_cls",
+            "filename": "paddle/PP-LCNet_x1_0_table_cls",
+        },
+        {
+            "repo_id": "PaddlePaddle/PP-LCNet_x1_0_textline_ori",
+            "filename": "paddle/PP-LCNet_x1_0_textline_ori",
+        },
+        {
+            "repo_id": "PaddlePaddle/PP-OCRv6_medium_det",
+            "filename": "paddle/PP-OCRv6_medium_det",
+        },
+        {
+            "repo_id": "PaddlePaddle/PP-OCRv6_medium_rec",
+            "filename": "paddle/PP-OCRv6_medium_rec",
+        },
+        {
+            "repo_id": "PaddlePaddle/RT-DETR-L_wired_table_cell_det",
+            "filename": "paddle/RT-DETR-L_wired_table_cell_det",
+        },
+        {
+            "repo_id": "PaddlePaddle/RT-DETR-L_wireless_table_cell_det",
+            "filename": "paddle/RT-DETR-L_wireless_table_cell_det",
+        }
+    ]
+
+    return config
+
+
+def optional_model_config() -> dict[str, list[dict[str, str]]]:
+    config = {
+        "transformers": [
+            {
+                "repo_id": "Qwen/Qwen3-VL-4B-Instruct",
+                "filename": "qwen/Qwen3-VL-4B-Instruct",
+            }
+        ],
+        "mlx": [
+            {
+                "repo_id": "mlx-community/Qwen3-VL-4B-Instruct-8bit",
+                "filename": "qwen_mlx/Qwen3-VL-4B-Instruct-8bit",
+            }
+        ]
+    }
+    return config
--- generate/download_model_test.py
+++ generate/download_model_test.py
@@ -1,67 +1,10 @@
 import unittest
 from generate.download_model import download_model
-from pkg.common import ensure_dir
-import os
-import pkg
-from paddleocr import PaddleOCRVL, PPStructureV3, LayoutDetection
 
 
 class TestDownloadModel(unittest.TestCase):
     def test_download(self):
         download_model(
-            repo_id="juliozhao/DocLayout-YOLO-DocStructBench",
-            filename="doclayout_yolo_docstructbench_imgsz1024.pt",
+            repo_id="PaddlePaddle/PP-DocLayoutV3",
+            filename="paddle_test/PP-DocLayoutV3",
         )
-
-    def test_paddle_ocrvl(self):
-        pipeline = PaddleOCRVL(device="cpu")
-
-        image_path = os.path.join(pkg.PdfTempDir, "test", "pdf_image", "paddleocr_vl_demo.png")
-        output_dir = os.path.join(pkg.PdfTempDir, "paddle_ocrvl")
-
-        ensure_dir(output_dir)
-
-        output = pipeline.predict(image_path)
-        for res in output:
-            res.print()  ## 打印预测的结构化输出
-            res.save_to_json(save_path=output_dir)
-            res.save_to_markdown(save_path=output_dir)
-
-    def test_ppstructurev3(self):
-        pipeline = PPStructureV3(
-            layout_detection_model_name="PP-DocLayout_plus-L",
-            device="cpu"
-        )
-
-        image_path = os.path.join(pkg.PdfTempDir,
-                                  "aws_2023_ba386fee-02ce-4f61-85d4-5e85926ce159",
-                                  "pdf_image",
-                                  "page_1.webp")
-        output_dir = os.path.join(pkg.PdfTempDir, "paddle_structure_v3")
-
-        ensure_dir(output_dir)
-
-        output = pipeline.predict(image_path)
-        for res in output:
-            res.print()  ## 打印预测的结构化输出
-            res.save_to_json(save_path=output_dir)
-            res.save_to_markdown(save_path=output_dir)
-
-    def test_pp_layout_detection(self):
-        pipeline = LayoutDetection(
-            model_name="PP-DocLayoutV3",
-            device="cpu"
-        )
-
-        image_path = os.path.join(pkg.PdfTempDir,
-                                  "aws_2023_ba386fee-02ce-4f61-85d4-5e85926ce159",
-                                  "pdf_image",
-                                  "page_1.webp")
-        output_dir = os.path.join(pkg.PdfTempDir, "paddle_layout_detection")
-
-        ensure_dir(output_dir)
-
-        output = pipeline.predict(image_path)
-        for res in output:
-            res.print()  ## 打印预测的结构化输出
-            res.save_to_json(save_path=output_dir)
