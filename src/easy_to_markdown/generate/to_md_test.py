5d31dfc45140b79327fdfd892f7a3c9780b0c59b
xuanjinliang <jinliang.xuan@mobvista.com>
2026-08-27 16:24:22 +0800

    add code

--- src/easy_to_markdown/generate/to_md_test.py
+++ src/easy_to_markdown/generate/to_md_test.py
@@ -8,8 +8,29 @@ from easy_to_markdown.pkg.enum_class import BlockType
 
 
 class ToMarkdown(unittest.TestCase):
-    def test_markdown_json_writer(self):
+    def test_markdown_json_writer1(self):
         file_dir = os.path.join(pkg.PdfTempDir, "aws_2024_cdn_24083b34-766a-48ad-9cdc-851744b1085c")
+        with open(os.path.join(file_dir, "layout_result.json"), "r", encoding="utf-8") as f:
+            data = json.load(f)
+
+        file_parsing_result_list = [FileParsingResult.model_validate(item) for item in data]
+
+        md_json_writer = MarkdownJsonWriter(pkg.MDDir)
+        md_json_info = md_json_writer.run(file_parsing_result_list)
+        print(md_json_info)
+
+        json_data = [r.model_dump() for r in md_json_info]
+        json_str = json.dumps(
+            json_data,
+            ensure_ascii=False,
+            indent=2
+        )
+
+        with open(os.path.join(pkg.MDDir, "md_result.json"), "w", encoding="utf-8") as f:
+            f.write(json_str)
+
+    def test_markdown_json_writer2(self):
+        file_dir = os.path.join(pkg.PdfTempDir, "aws_2023_ba386fee-02ce-4f61-85d4-5e85926ce159")
         with open(os.path.join(file_dir, "layout_result.json"), "r", encoding="utf-8") as f:
             data = json.load(f)
 
